c9161039feccf1db87de6a363e054c751f385c72
kim3076 <46684249+kim3076@users.noreply.github.com>
2020-07-05 10:29:37 -0400

    Add files via upload

---
+++ Suppport_Vector_Regression.R
@@ -0,0 +1,26 @@
+dataset = read.csv('Position_Salaries.csv')
+dataset = dataset[2:3]
+
+
+
+#install.packages('e1071')
+
+library(e1071)
+regressor = svm(formula = Salary ~.,
+                data = dataset,
+                type='eps-regression')
+
+y_pred = predict(regressor, data.frame(Level = 6.5))
+
+
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x=dataset$Level, y= dataset$Salary),
+             color = 'red') +
+  geom_line(aes(x=dataset$Level, y = predict(regressor, newdata = dataset)),
+            color = 'blue') +
+  ggtitle('Support Vector Model2') + 
+  xlab('Level') + 
+  ylab('Salary')
+
+
